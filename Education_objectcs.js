f45c133bc4e8f8fa3b88d94684d9c2f21fde592c
Danylo <danilovityk@gmail.com>
2023-04-21 15:03:09 -0400

    sample text changed

--- Education_objectcs.js
+++ Education_objectcs.js
@@ -4,7 +4,7 @@ window.products = [
         title: "Advanced diploma",
         institution: "Seneca college",
         description:
-            "The Glock 19 is a polymer-framed, striker-fired semi-automatic pistol, designed for versatile use and renowned for its durability and ease of use.",
+            "The sun was setting over the horizon, casting a warm glow over the rugged landscape. As the day drew to a close, the animals of the forest began to stir.",
         categories: ["Ed"],
         imageUrl: "https://aspiresquare.com/wp-content/uploads/2022/03/Seneca-College.jpg",
         years: "2022 - 2025 (expected)"
@@ -16,7 +16,7 @@ window.products = [
         title: "IT Support Certificate",
         institution: "Google",
         description:
-            "The Glock 19 is a polymer-framed, striker-fired semi-automatic pistol, designed for versatile use and renowned for its durability and ease of use.",
+            "Birds chirped their final songs of the day, while rabbits darted about in search of a place to sleep for the night.",
         categories: ["Ed"],
         imageUrl: "https://dam.nmhmedia.sk/image/cd21770f-6da6-45d1-957a-7c183cb556f6_phpbbgdhj.jpg/960/540",
         years: "2023"
@@ -26,7 +26,7 @@ window.products = [
         title: "High school diploma",
         institution: "Gymnasium Prestige",
         description:
-            "The Glock 19 is a polymer-framed, striker-fired semi-automatic pistol, designed for versatile use and renowned for its durability and ease of use.",
+            "A gentle breeze rustled the leaves of the trees, creating a soothing sound that added to the peaceful atmosphere. ",
         categories: ["Ed"],
         imageUrl: "https://images.adsttc.com/media/images/515b/0985/b3fc/4b29/d600/00b3/large_jpg/2edisonacadbldg2880wphoto.jpg?1364920696",
         years: "2015 - 2022"
